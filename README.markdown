6bbda3d2f55e51bb62e6999b95367ba45d12593a
Alex Dunae <alex@dunae.ca>
2010-11-29 11:49:53 -0800

    Initial commit

---
+++ README.markdown
@@ -0,0 +1,26 @@
+# Preview for Campaign Monitor Template Tags
+
+This little bit of JavaScript makes designing with Campaign Monitor's template tags much easier.
+
+It replaces all Campaign Monitor's template tags with their fallback defaults.  You can preview the final output instead of all those angle brackets and broken images.
+
+On the left of the screenshot you can see the usual markup madness.  On the right you see the results of the script.
+
+![Screenshot](screenshot.jpg)
+
+Specifically, this script:
+
+* replaces `title` and `description` tags with their default text
+* replaces `img` tags with their default images
+* replaces the personalization (mail merge) tags with their fallback text
+* duplicates each `repeater` so you can see how multiples will look
+  
+## Using the script
+
+Link the script at the end of your HTML email file and view the file in your browser.
+
+  <script src="cm-preview.js"></script>
+
+This is a rough-and-ready little script, lightly tested in Safari, Chrome and Firefox.  It's released under the MIT license.  Patches and improvements are most welcome.
+
+Created by Alex Dunae at [Dialect](http://dialect.ca/).
---
+++ cm-preview.js
@@ -0,0 +1,54 @@
+/*!
+	Preview script for Campaign Monitor template tags.
+	
+	See https://github.com/alexdunae/cm-preview/ for usage and the latest source.
+	
+	Copyright (c) 2010 Alex Dunae. See LICENSE for details.
+	
+	By Alex Dunae at Dialect (http://dialect.ca/).  Enjoy!
+*/
+
+var body = document.getElementsByTagName('body');
+
+// show default text
+// single quoted
+repl = body[0].innerHTML.replace(/\&lt;\$(title|description)[\s]+([\w]+='[\w]+'[\s]*)?default='(.[^']+)'[\s]*([\w]+='[\w]+'[\s]*)?\$\&gt;/ig, "$3");
+// double quoted
+repl = repl.replace(/\&lt;\$(title|description)[\s]+([\w]+="[\w]+"[\s]*)?default="(.[^"]+)"[\s]*([\w]+="[\w]+"[\s]*)?\$\&gt;/ig, "$3");
+
+// and show personalization fallbacks
+repl = repl.replace(/\[(\w+),fallback=(.[^\]]+)\]/ig, "$2");
+
+
+body[0].innerHTML = repl;
+
+// show default images
+var imgs = document.getElementsByTagName('img');
+for(var i = 0; i < imgs.length; i++) {
+	var src = imgs[i].getAttribute('src', 2);
+	if (/\$imagesrc\$/i.test(src)) {
+		// unescape attributes for Firefox
+		if (src.indexOf('%') !== false) {
+			src = unescape(src);
+		}
+
+		src = src.match(/default=['"](.[^'"]+)/i);
+		if (src && src[1]) {
+			imgs[i].setAttribute('src', src[1]);
+		}
+	}
+}
+
+// show multiple repeaters
+var repeaters = document.getElementsByTagName('repeater');
+var clones = [];
+for(var i = 0; i < repeaters.length; i++) {
+	clones[i] = repeaters[i].cloneNode(true);
+}
+
+for(var i = 0; i < clones.length; i++) {
+	repeaters[i].insertBefore(clones[i], null);
+}
+
+
+
