91db4a26935bd05441f6a545d545eb2ff4eb4951
Alex Dunae <alex@dunae.ca>
2010-12-07 09:24:01 -0800

    Add support for date tags

--- cm-preview.js
+++ cm-preview.js
@@ -19,6 +19,16 @@ repl = repl.replace(/\&lt;\$(title|description)[\s]+([\w]+="[\w]+"[\s]*)?default
 // and show personalization fallbacks
 repl = repl.replace(/\[(\w+),fallback=(.[^\]]+)\]/ig, "$2");
 
+// dates
+var dt = new Date(), 
+    days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+    months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+repl = repl.replace(/\&lt;\$currentday\$\&gt;/ig, dt.getDate());
+repl = repl.replace(/\&lt;\$currentdayname\$\&gt;/ig, days[dt.getDay()]);
+repl = repl.replace(/\&lt;\$currentmonth\$\&gt;/ig, dt.getMonth() + 1);
+repl = repl.replace(/\&lt;\$currentmonthname\$\&gt;/ig, months[dt.getMonth()]);
+repl = repl.replace(/\&lt;\$currentyear\$\&gt;/ig, dt.getFullYear());
 
 body[0].innerHTML = repl;
 
